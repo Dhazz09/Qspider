eb1ddb9b90f298dca2d2b94adee4582f468cdd17
DHAVASIDHARAN S <dhavasidharan0907@gmail.com>
2025-09-01 21:12:12 +0530

    Add files via upload

---
+++ TEST/pylove.py
@@ -0,0 +1,8 @@
+#Wap to count the number of words in a string.
+#Input 1: s = “ I love coding in Python ”
+#Output 1: 5
+s="I love coding in python"
+print("Given INPUT")
+print(s)
+a=list(s.split())
+print(len(a))
