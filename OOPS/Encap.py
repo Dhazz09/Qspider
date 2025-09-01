a7dd1c7a54927ef76c9de712a4cc864983d2bf8d
DHAVASIDHARAN S <dhavasidharan0907@gmail.com>
2025-09-01 09:20:36 +0530

    Delete OOPS/Encap.py

--- OOPS/Encap.py
+++
@@ -1,9 +0,0 @@
-class Mobile:
-    def __init__(self,brand,battery,pin):
-        self.brand=brand
-        self._battery=battery   #PROTECTED
-        self.__pin=pin          #PRIVATE
-ob=Mobile("Iphone",100,9211)
-#print(ob.__pin)
-print(ob._battery)
-print(ob.brand)
